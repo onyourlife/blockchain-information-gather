adb4cd5721bb17509f4067240bb7c05d4101307b
Kanghyo Lee <onyourlife@gmail.com>
2019-09-29 16:40:14 +0900

    Add Function : input the data on excel file.

--- excel.py
+++ excel.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import datetime
 from openpyxl import Workbook
-from openpyxl.styles import Font, Alignment
+from openpyxl.styles import Font, Alignment, PatternFill
 
+# 폰트 설정
 def setFont(cell):
     cell.font = Font(name='맑은 고딕', size=10, bold=True)
 
+# 가운데 정렬
 def setAlign(cell):
     cell.alignment = Alignment(horizontal='center', vertical='center')
 
-wb = Workbook()
-ws = wb.active
-ws.freeze_panes = 'A3'
+# 음영 색 지정
+yellowFill = PatternFill(start_color='FFFFFF00',
+            end_color='FFFFFF00',
+            fill_type='solid')
 
-title = ['A3','B3','C3','D3','E3']
-title_content = ['ID', 'NAME', 'ICO 시작일', 'ICO 종료일', 'ICO 총 상장액']
-ws.merge_cells('A1:E1')
+def makeExcel(coinList):
 
-for i in range(len(title)):
-    ws[title[i]] = title_content[i]
+    # 워크시트 생성
+    wb = Workbook()
+    ws = wb.active
 
-for i in range(len(title)):
-    setFont(ws[title[i]])
+    # 행 고정
+    ws.freeze_panes = 'A4'  
+    ws.merge_cells('A1:F1')
+    ws['A1'] = 'ICO 현황'
 
-for i in range(len(title)):
-    setAlign(ws[title[i]])
+    title = ['A3','B3','C3','D3','E3','F3']
+    title_content = ['ID', 'NAME', 'ICO 시작일', 'ICO 종료일', 'ICO 총 상장액', '통화']
 
-wb.save('test.xlsx')
+    # 제목 행 입력
+    for i in range(len(title)):
+        ws[title[i]] = title_content[i]
 
+        # set the width of the column 
+        ws.column_dimensions[title[i][:1]].width = 20
+        
+        # 지정된 음영 색으로 음역 색칠하기
+        ws[title[i]].fill = yellowFill
+        setFont(ws[title[i]])
+        setAlign(ws[title[i]])
+
+    for coin in coinList:
+        # parsing
+        print(coin)
+        coin_id, coin_name, ico_sdate, ico_edate, ico_rasied, currency = coin.split('|')
+        ws.append([coin_id, coin_name, ico_sdate[:10], ico_edate[:10], ico_rasied, currency])
+    
+    wb.save('test.xlsx')
--- search_ico.py
+++ search_ico.py
@@ -1,30 +1,58 @@
+'''
+ *************************************************
+ * bcStatus - Blockchain Statics Gahter
+ * Author: Kanghyo Lee <onyourlife@gmail.com>
+ * Copyright (c): 2019, all rights reserved
+ * Version: 0.0.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ *
+ * You may contact the author of bcStatus by e-mail at:
+ * onyourlife@gmail.com
+ *
+ * The latest version of bcStatus can be obtained from:
+ * https://github.com/onyourlife/blockchain-information-gather
+ *************************************************
+'''
+
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import requests
 import datetime
+import excel
+from tqdm import tqdm
+from threading import Thread
 from bs4 import BeautifulSoup
 
+# get coin list from coingecko
 coinList = requests.get("https://api.coingecko.com/api/v3/coins/list").json()
 
-fw = open('ICO.txt','w')
+data = []
 
 # Extract Coin ID
-for i in range(len(coinList)) :    
+for i in tqdm(range(len(coinList))) :    
     coin = requests.get("https://api.coingecko.com/api/v3/coins/"+coinList[i]['id']+"?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true").json()
     try:
         if coin['ico_data']['country_origin'] != "KR" :
-            fw.write("ID : " + coin['id'] + ", NAME : " + coin['name'] + ", ICO 시작일 : " + str(coin['ico_data']['ico_start_date']) + ", ICO 종료일 : " + str(coin['ico_data']['ico_end_date']) + ", ICO 총 상장액 : " + str(coin['ico_data']['total_raised']) + " " + str(coin['ico_data']['total_raised_currency'])+"\n")                    
-            print("ID : " + coin['id'])
-            #print("ID : " + coin['id'] + ", NAME : " + coin['name'] + ", ICO 시작일 : " + str(coin['ico_data']['ico_start_date']) + ", ICO 종료일 : " + str(coin['ico_data']['ico_end_date']) + ", ICO 총 상장액 : " + str(coin['ico_data']['total_raised']) + " " + str(coin['ico_data']['total_raised_currency']))
-            '''
-            print("ID : " + coin['id'] + ", " + "NAME : " + coin['name'])
-            print("ICO 시작일 : " + str(coin['ico_data']['ico_start_date']))
-            print("ICO 종료일 : " + str(coin['ico_data']['ico_end_date']))
-            print("ICO 총 상장액 : " + str(coin['ico_data']['total_raised'] + " " + str(coin['ico_data']['total_raised_currency'])))
-            '''
+            data.append(coin['id'] + "|" + coin['name'] + "|" + str(coin['ico_data']['ico_start_date']) + "|" + str(coin['ico_data']['ico_end_date']) + "|" + str(coin['ico_data']['total_raised']) + "|" + str(coin['ico_data']['total_raised_currency']))                       
     except:
         pass
 
-fw.close()
+# print(data)
+
+# Save the result on Excel file
+excel.makeExcel(data)
+
 print("[*] Finished.")
